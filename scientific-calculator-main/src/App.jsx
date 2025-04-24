36d9be61e71eac35284e11c7007c1e48d2692131
Nilesh Parab <95012517+nilesh2003parab@users.noreply.github.com>
2025-04-24 23:26:56 +0530

    initial

---
+++ scientific-calculator-main/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+      <p className="developer">
+        scientific calculator <span>optifyx</span>
+      </p>
+    </div>
+  );
+};
+
+export default App;
